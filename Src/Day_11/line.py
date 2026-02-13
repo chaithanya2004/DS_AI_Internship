9a4794763c9103e615512b7672bba35d81a6c491
chaithanya <116102108+chaithanya2004@users.noreply.github.com>
2026-02-13 11:57:12 +0530

    Add files via upload

---
+++ Src/Day_11/line.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 11:26:34 2026
+
+@author: User
+"""
+import matplotlib.pyplot as plt
+months = [1, 2, 3, 4, 5];
+revenue = [2000, 4500, 4000, 7500, 9000];
+plt.plot(months,revenue);
+plt.title("Monthly Revenue Growth");
+plt.xlabel("Month")
+plt.ylabel("Revenue in USD")
+plt.show();
